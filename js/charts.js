b66e64d9f431b659d0f3f8500d80c933f8d6422c
Dmitriy An <an.dmitriy01@gmail.com>
2017-07-29 17:03:03 +0200

    Almost finished all the charts

--- js/charts.js
+++ js/charts.js
@@ -52,20 +52,20 @@ var chartTraffic = new Chart(ctx, {
 var ctx2 = document.getElementById('chartTrafficDaily').getContext('2d');
 var chartTrafficDaily = new Chart(ctx2, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'bar',
 
     // The data for our dataset
     data: {
-        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "17-3", "4-10", "11-17", "18-24", "25-31"],
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
         datasets: [{
             label: "Traffic",
             lineTension: 0,
-            backgroundColor: 'rgba(116,119,191, .35)',
+            backgroundColor: 'rgb(116,119,191)',
             borderColor: 'rgb(116,119,191)',
+            hoverBackgroundColor: '#81c98f',
+            hoverBorderColor: '#81c98f',
             borderWidth: 2,
-            pointBackgroundColor: '#fff',
-            pointRadius: 6,
-            data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 2000, 1500, 2000],
+            data: [50, 75, 150, 100, 200, 175, 80],
         }]
     },
 
@@ -91,7 +91,7 @@ var chartTrafficDaily = new Chart(ctx2, {
          yAxes: [{
             ticks: {
                 suggestedMin: 0,
-                suggestedMax: 2500,
+                suggestedMax: 250,
                 fontColor: '#676666',
             }
          }],
@@ -103,49 +103,30 @@ var chartTrafficDaily = new Chart(ctx2, {
 var ctx3 = document.getElementById('chartUsers').getContext('2d');
 var chartTrafficDaily = new Chart(ctx3, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'doughnut',
 
     // The data for our dataset
     data: {
-        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "17-3", "4-10", "11-17", "18-24", "25-31"],
+      labels: ['Phones', 'Tablets', 'Descktop'],
         datasets: [{
-            label: "Traffic",
-            lineTension: 0,
-            backgroundColor: 'rgba(116,119,191, .35)',
-            borderColor: 'rgb(116,119,191)',
-            borderWidth: 2,
-            pointBackgroundColor: '#fff',
-            pointRadius: 6,
-            data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 2000, 1500, 2000],
+           data: [16,
+                  14,
+                  80
+            ],
+
+           backgroundColor: [
+            '#81c98f',
+            '#74b1bf',
+            '#7477BF'
+          ],
         }]
     },
 
     // Configuration options go here
     options: {
       legend: {
-         display: false,
+         // display: false,
       },
 
-
-
-      scales: {
-         xAxes: [{
-            gridLines: {
-               offsetGridLines: true,
-            },
-
-            ticks: {
-               fontColor: '#676666',
-            }
-         }],
-
-         yAxes: [{
-            ticks: {
-                suggestedMin: 0,
-                suggestedMax: 2500,
-                fontColor: '#676666',
-            }
-         }],
-      }
    }
 });
